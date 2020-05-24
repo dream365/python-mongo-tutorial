c6f5ff3268956aff56b86fae3b643d88fbd4cebb
dream365 <kosewon_@naver.com>
2020-05-24 13:01:33 +0900

    Create index for product_id in review collection

--- query/query_test.py
+++ query/query_test.py
@@ -1,4 +1,5 @@
 from utils import db_utils as dbu
+import pymongo
 import pprint
 
 category_collection = dbu.get_collection('category')
@@ -8,6 +9,8 @@ order_collection = dbu.get_collection('order')
 review_collection = dbu.get_collection('review')
 
 def find_query_test():
+    review_collection.create_index([('product_id', pymongo.ASCENDING)])
+
     sample_product = product_collection.find_one({"slug": "wheel-barrow-9092"})
     category = category_collection.find_one({"_id": sample_product['main_cat_id']})
     reviews = review_collection.find({"product_id": sample_product['_id']})
@@ -25,7 +28,7 @@ def find_query_test():
     print('===============================================================================')
     for review in reviews:
         pprint.pprint(review)
-    print('===============================================================================')
+        print('===============================================================================')
 
 if __name__ == "__main__":
     find_query_test()
